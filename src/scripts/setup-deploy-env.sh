2b6fe9e269d5ebbbf81ce0238965125b93c151bb
Martin PAUCOT <m.paucot@kmp.agency>
2022-10-08 17:41:38 +0200

    update

--- src/scripts/setup-deploy-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "export PARAM_PROJECT='$PARAM_PROJECT'" >> "$BASH_ENV"
-echo "export PARAM_ENVIRONMENT='$PARAM_ENVIRONMENT'" >> "$BASH_ENV"
-echo "export PARAM_URL='$PARAM_URL'" >> "$BASH_ENV"
